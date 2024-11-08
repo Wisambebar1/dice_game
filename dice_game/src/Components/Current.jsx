53b9a732d6dd8683430f3d71c49eb607a88eb494
Wisambebar1 <wisambebar@gmail.com>
2024-11-08 18:09:53 +0200

    addded useffect to sum up the values

--- dice_game/src/Components/Current.jsx
+++ dice_game/src/Components/Current.jsx
@@ -1,8 +1,10 @@
-import React ,{ useState } from 'react'
+import React ,{ useState,useEffect } from 'react'
 const Current=({number1,number2})=>{
+    let [total,setTotal]=useState(0);
+    useEffect(()=>{setTotal(total=>total+number1+number2)},[number1,number2])
     return(
         <div>
-            <h1>{number1+number2}</h1>
+            <h1>{total}</h1>
         </div>
     )
 }
